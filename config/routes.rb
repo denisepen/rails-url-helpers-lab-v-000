7f4549783caa239b5fb7e23bbdc504bb7e12ea96
Denise Pendleton <dpendl1@hotmail.com>
2018-05-14 23:29:39 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :show]
+  resources :students, only: [:index, :show, :activate]
 
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   # the route, we'll keep it explicit for now and refactor later.
   # get "students/:id", to: "students#show"
   # get "students/:id/activate", to: "students#activate"
-  get "activate/student", to: "students#activate"
+  # get "activate/student", to: "students#activate"
 end
